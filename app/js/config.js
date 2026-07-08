6f80c1ae0a1f53d7dee0b60d681e00c6ef8b304e
Sergio <103077339+SergioJRosales@users.noreply.github.com>
2026-07-08 16:11:13 -0300

    Update config.js

--- app/js/config.js
+++ app/js/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  appsScriptUrl: 'PEGAR_AQUI_TU_WEB_APP_URL'
+  appsScriptUrl: 'https://script.google.com/a/macros/autotrato.com.ar/s/AKfycbyc5v6fRL9_z4d2ZGexmiba3GFB6FhYlNkwLyuDk24qfYaiCnrNJ7R8GF3XeUT-v19dvw/exec'
 };
